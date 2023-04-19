4e89a474943951822b8b7e28b9f4b381952ef888
maxxberg <max.git@mx-berg.de>
2023-04-19 22:34:37 +0200

    added oh-my-zsh installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,18 @@
 BASEDIR="$(dirname ${PWD}/${0})"
 BASEDIR="$(echo ${BASEDIR} | sed -e 's/\/\.$//g' )"
 
-echo "${BASEDIR}"
 
-# oh-my-zsh
+## oh-my-zsh
+# install oh-my-zsh if not exists
+if [[ ! -d "${HOME}/.oh-my-zsh" ]]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# copy *.zsh files to custom
 if [[ -n "${ZSH_CUSTOM}" ]]; then
 	cp -rf "${BASEDIR}/.oh-my-zsh/custom" "${ZSH_CUSTOM}"
 fi
 
-# nvim
+## nvim
+#copy nvim files to their directory
 cp -r "${BASEDIR}/.config/nvim" "${XDG_CONFIG_HOME:-$HOME/.config}"
