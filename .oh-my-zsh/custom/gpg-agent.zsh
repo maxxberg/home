1e37c45c6eedd0a1dde8ec4b235e25eb8067cc46
maxxberg <max.git@mx-berg.de>
2023-04-20 00:06:27 +0200

    fixed gpg-agent missing path

--- .oh-my-zsh/custom/gpg-agent.zsh
+++ .oh-my-zsh/custom/gpg-agent.zsh
@@ -1,3 +1,7 @@
+if [[ ! -d "${HOME}/.gnupg" ]]; then
+	mkdir -p "${HOME}/.gnupg"
+fi
+
 export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
