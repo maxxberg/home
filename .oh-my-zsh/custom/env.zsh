fa8793e1cc343421dda7deb5c9b816bd9efbf8d3
maxxberg <max.git@mx-berg.de>
2023-05-16 21:24:59 +0200

    Change back to ssh-agent

--- .oh-my-zsh/custom/env.zsh
+++ .oh-my-zsh/custom/env.zsh
@@ -1,1 +1,7 @@
 export PATH=$PATH:$HOME/.local/bin/scripts
+export SSH_AUTH_SOCK=~/.ssh/ssh-agent.sock
+ssh-add -l 2>/dev/null >/dev/null
+if [ $? -ge 2 ]; then
+  rm -rf "$SSH_AUTH_SOCK"
+  ssh-agent -a "$SSH_AUTH_SOCK" >/dev/null
+fi
