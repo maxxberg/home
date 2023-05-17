695dcc1444849ea5e048c6fa99a0892727d4a4e5
maxxberg <max.git@mx-berg.de>
2023-05-17 16:33:02 +0200

    updated ansible.zsh

--- .oh-my-zsh/custom/ansible.zsh
+++ .oh-my-zsh/custom/ansible.zsh
@@ -1,6 +1,23 @@
+maxxberg.ops.tmpl () {
+  podman run -it --rm --pull=newer -v ~/.ssh:/root/.ssh "ghcr.io/maxxberg/maxxberg.ops:${1}" "${@:2}"
+}
+
+maxxberg.ops.prod_raw () {
+  maxxberg.ops.tmpl main "${@}"
+}
+
 maxxberg.ops.prod () {
 	if [ -z $1 ]; then
 		return
 	fi
-	podman run -it --rm --pull=newer -v ~/.ssh:/root/.ssh ghcr.io/maxxberg/maxxberg.ops:main ansible-playbook --ask-vault-pass -i inventory/prod.yml playbooks/$1.yaml
+	maxxberg.ops.prod_raw ansible-playbook --ask-vault-pass -i inventory/prod.yml "playbooks/${1}.yaml"
+}
+
+
+maxxberg.ops.dev_raw () {
+  maxxberg.ops.tmpl devel "${@}"
+}
+
+maxxberg.ops.dev () {
+  maxxberg.ops.dev_raw ansible-playbook --ask-vault-pass -i inventory/dev.yml "playbooks/${1}.yml"
 }
